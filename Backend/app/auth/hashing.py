8f113dc35c7ddfcefff27a7f94693e5a73758b07
Divyaraj <divyarajjadav46@gmail.com>
2026-07-23 21:35:02 +0530

    resolved dependency issue regarding bcrypt

--- Backend/app/auth/hashing.py
+++ Backend/app/auth/hashing.py
@@ -1,18 +1,37 @@
-from passlib.context import CryptContext
+# Original Code 
+# from passlib.context import CryptContext
 
 
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
+# pwd_context = CryptContext(
+#     schemes=["bcrypt"],
+#     deprecated="auto"
+# )
+
+
+# def hash_password(password: str) -> str:
+#     return pwd_context.hash(password)
+
+
+# def verify_password(
+#     plain_password: str,
+#     hashed_password: str
+# ) -> bool:
+#     return pwd_context.verify(plain_password, hashed_password)
+
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
 
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return password_hash.hash(password)
 
 
 def verify_password(
     plain_password: str,
-    hashed_password: str
+    hashed_password: str,
 ) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return password_hash.verify(
+        plain_password,
+        hashed_password,
+    )
